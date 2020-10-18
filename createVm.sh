b09918ce6c2ff9da04ebbc5b50532f3bbe8beed1
David Riusech <v1X3Q0@gmail.com>
2020-10-18 04:40:00 -0400

    create changes

--- createVm.sh
+++ createVm.sh
@@ -10,9 +10,36 @@ python3 xnu-qemu-arm64-tools/bootstrap_scripts/asn1kerneldecode.py kernelcache.r
 python3 xnu-qemu-arm64-tools/bootstrap_scripts/decompress_lzss.py kernelcache.release.n66.asn1decoded kernelcache.release.n66.out
 python3 xnu-qemu-arm64-tools/bootstrap_scripts/asn1dtredecode.py Firmware/all_flash/DeviceTree.n66ap.im4p Firmware/all_flash/DeviceTree.n66ap.im4p.out
 
+fileST=()
+fileNM=()
+ramFS=()
+ipswFS=()
+
 for filename in ./*.dmg; do
     myfilesize=$(wc -c "${filename}" | awk '{print $1}')
+    if [ ${#fileST[@]} -eq 0 ]
+    then
+        fileST+=("$myfilesize")
+        fileNM+=("$filename")        
+    elif [ $myfilesize -ge ${fileST[ ${#fileST[@]} - 1 ]} ]
+    then
+        fileST+=("$myfilesize")
+        fileNM+=("$filename")        
+    elif [ $myfilesize -le ${fileST[ 0 ]} ]
+    then
+        fileST=("$myfilesize" ${fileST[@]})
+        fileNM=("$filename" ${fileNM[@]})
+    else
+        ramFS=${filename}
+    fi
 done
 
+if [ ${#fileST[@]} -eq 3]
+then
+    ramFS=${fileNM[ ${#fileNM[@]} - 2 ]}
+fi
 
-python3 xnu-qemu-arm64-tools/bootstrap_scripts/asn1rdskdecode.py ./048-32651-104.dmg ./048-32651-104.dmg.out
+ipswFS=${fileNM[ ${#fileNM[@]} - 1 ]}
+
+
+python3 xnu-qemu-arm64-tools/bootstrap_scripts/asn1rdskdecode.py ${ramFS} ${ramFS}.out
